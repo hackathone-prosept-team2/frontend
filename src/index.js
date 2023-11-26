f79f753103582ebc188d720e894af43385eb3417
Максим Таланов <mail@maxtalanov.ru>
2023-11-26 20:24:16 +0300

    feat: Add provider for accessing the store
    Introduce the Provider component to grant access to the Redux store throughout the application.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App/App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Store/store.js';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ BrowserRouter>
   </ React.StrictMode>
 );
