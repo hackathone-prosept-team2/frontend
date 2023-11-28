462e4e52a96fcb56b91a76245e805ef21dcc9cbd
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-11-28 14:11:50 +0300

    style/header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,13 +5,20 @@ function Header() {
 
   return (
     <header className="header">
-      <Link to='/' className="header__logo">PROSEPT</Link>
-      {location === '/users/create/' &&
-        <Link to='/users/login/' className="header__link">Войти</Link>}
-      {location === '/users/login/' &&
-        <Link to='/users/create/' className="header__link">Регистрация</Link>}
-      {location === '/' &&
-        <Link to='/logout' className="header__link">Выйти</Link>}
+      <Link to='/' className="header__logo"></Link>
+      <div className="header__container">
+        <div className="header__links">
+          {location === '/users/create/' &&
+            <Link to='/users/login/' className="header__link">Войти</Link>}
+          {location === '/users/login/' &&
+            <Link to='/users/create/' className="header__link">Регистрация</Link>}
+          {location === '/' &&
+            <Link to='/logout' className="header__link">Выйти</Link>}
+          {location === '/logout' &&
+            <Link to='/users/login/' className="header__link">Войти</Link>}
+        </div>
+      </div>
+
     </header>
   )
 }
