ce7dd6768aa2c5792df4aad57ffd498dd44e81a1
Максим Таланов <mail@maxtalanov.ru>
2023-11-28 17:27:41 +0300

    refactor: imports for better organization

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Components/App/App.jsx';
+import { App } from 'Components';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './Store/store.js';
+import { store } from 'Store';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
