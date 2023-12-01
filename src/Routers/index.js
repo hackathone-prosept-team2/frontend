3fbd707933ba41d83953c0e1396d3a844a6bc222
Максим Таланов <mail@maxtalanov.ru>
2023-12-01 14:07:06 +0300

    Add manifest list of routers.

---
+++ src/Routers/index.js
@@ -0,0 +1,24 @@
+const Routers = {
+    DEFAULT: {
+        path: "/",
+        title: "Главная",
+    },
+    AUTH_SIGNUP: {
+        path: "/signup",
+        title: "Регистрация",
+    },
+    AUTH_SIGNIN: {
+       path: "/signin",
+       title: "Авторизация",
+    },
+    HOME: {
+        path: "/home",
+        title: "Главная",
+    },
+    NOT_FOUND: {
+        path: "*",
+        title: "Ошибка 404",
+    },
+}
+
+export default Routers
