fc525983b79a0a0b6571949564c29a8b82ea3cd5
Максим Таланов <mail@maxtalanov.ru>
2023-12-02 16:29:32 +0300

    Add submit button to authentication forms

--- src/Components/Auth/Login/Login.jsx
+++ src/Components/Auth/Login/Login.jsx
@@ -1,14 +1,14 @@
 import { Input } from "Ui-Kit"
+import { Button } from "Ui-Kit"
 
 import "../Auth.css"
 
-
 export const Login = () => {
-
-    return (
-        <form className="Auth__Form">
-            <Input label="Логин" type="text" autoComplete="username" />
-            <Input label="Пароль" type="password" autoComplete="current-password" />
-        </form>
-    )
-} 
+  return (
+    <form className="Auth__Form">
+      <Input label="Логин" type="text" autoComplete="username"/>
+      <Input label="Пароль" type="password" autoComplete="current-password"/>
+      <Button name="Войти"/>
+    </form>
+  )
+}
--- src/Components/Auth/Registration/Registration.jsx
+++ src/Components/Auth/Registration/Registration.jsx
@@ -1,12 +1,13 @@
-import { Input } from "Ui-Kit"
+import {Input} from "Ui-Kit"
+import { Button } from "Ui-Kit"
 
 export const Registration = () => {
 
-
-    return (
-        <form className="Auth__Form">
-            <Input label="Логин" type="text" />
-            <Input label="Пароль" type="password" />
-        </form>
-    )
+  return (
+    <form className="Auth__Form">
+      <Input label="Логин" type="text"/>
+      <Input label="Пароль" type="password"/>
+      <Button name="Зарегистрироваться"/>
+    </form>
+  )
 }
