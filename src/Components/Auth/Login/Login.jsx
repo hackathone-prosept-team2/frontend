cdf11f8dfbf4eed23f53ca100f56d789ffb4c433
Максим Таланов <mail@maxtalanov.ru>
2023-12-03 20:40:20 +0300

    feat(use hook form): Integrate react-hook-form into auth forms
    Registration component
    Authorization component

--- src/Components/Auth/Login/Login.jsx
+++ src/Components/Auth/Login/Login.jsx
@@ -1,13 +1,34 @@
 import { Input, Button } from "Ui-Kit"
+import { useForm } from "react-hook-form"
 
 import "../Auth.css"
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
-    <form className="Auth__Form">
-      <Input label="Логин" type="text" autoComplete="username"/>
-      <Input label="Пароль" type="password" autoComplete="current-password"/>
-      <Button name="Войти"/>
+    <form className="Auth__Form" onSubmit={handleSubmit(onSubmit)}>
+      <Input 
+        label="Логин" 
+        type="text" 
+        autoComplete="username"
+        register={{...register("login")}}
+      />
+
+      <Input 
+        label="Пароль" 
+        type="password" 
+        autoComplete="current-password"
+        register={{...register("password")}}
+      />
+
+      <Button type="submit" name="Войти"/>
     </form>
   )
 }
--- src/Components/Auth/Registration/Registration.jsx
+++ src/Components/Auth/Registration/Registration.jsx
@@ -1,14 +1,34 @@
 import {Input, Button} from "Ui-Kit"
+import { useForm } from "react-hook-form"
 
 import "../Auth.css"
 
 export const Registration = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (registrationData) => console.log(registrationData)
 
   return (
-    <form className="Auth__Form">
-      <Input label="Логин" type="text"/>
-      <Input label="Пароль" type="password"/>
-      <Button name="Зарегистрироваться"/>
+    <form className="Auth__Form" onSubmit={handleSubmit(onSubmit)}>
+      <Input 
+        label="Логин" 
+        type="text" 
+        autoComplete="username"
+        register={{...register("login")}}
+      />
+
+      <Input 
+        label="Пароль" 
+        type="password" 
+        autoComplete="current-password"
+        register={{...register("password")}}
+      />
+
+      <Button type="submit" name="Зарегистрироваться" />
     </form>
   )
 }
