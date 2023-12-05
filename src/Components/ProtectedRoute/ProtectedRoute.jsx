cd8facb4ee401e0ebb9ab5e24509f3ab5fa51684
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-12-05 12:38:33 +0300

    pages/notfound

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useSelector } from "react-redux" 
+import { useSelector } from "react-redux"
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
-    const { isAuthenticated } = useSelector((state)=> state.auth)
+    //const { isAuthenticated } = useSelector((state)=> state.auth)
+
+  const isAuthenticated = true
 
     return isAuthenticated ? <Component {...props} /> : <Navigate to="/signin" replace />
   };
-  
+
   export default ProtectedRoute;
--- src/Pages/NotFoundPage/NotFoundPage.js
+++ src/Pages/NotFoundPage/NotFoundPage.js
@@ -1,8 +1,31 @@
-import { useChangeTitle } from "Hooks"
+import {useChangeTitle} from "Hooks"
+import {useNavigate} from 'react-router-dom';
 
-const NotFoundPage = ({ title="Ошибка 404" }) => {
-    useChangeTitle(title)
-    return(<>Страница не найдена</>)
+const NotFoundPage = ({title = "Ошибка 404"}) => {
+  useChangeTitle(title)
+  const navigate = useNavigate();
+
+  function openThePreviousPage() {
+    navigate(-1);
+  }
+
+  return (
+    <section className="not-found">
+      <h2 className="not-found__title">
+        404
+      </h2>
+      <p className="not-found__text">
+        Страница не найдена
+      </p>
+      <button
+        className="not-found__button"
+        type="button"
+        aria-label="back"
+        onClick={openThePreviousPage}>
+        Назад
+      </button>
+    </section>
+  )
 }
 
 export default NotFoundPage
