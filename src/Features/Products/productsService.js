43a59fd36785b33e82e2c568aca3b8333bb0d0dc
Максим Таланов <mail@maxtalanov.ru>
2023-12-07 23:28:48 +0300

    feat(services): Complete the file for the getProductAll method

--- src/Features/Products/productsService.js
+++ src/Features/Products/productsService.js
@@ -1,13 +1,25 @@
 import axios from "axios"
-import { API_URL, getDataFromLocalStorage } from "Utils"
+import { 
+    API_URL, 
+    getDataFromLocalStorage,
+    urlParamsGenerate, 
+} from "Utils"
+
+const token = `Token ${getDataFromLocalStorage("user")?.auth_token}`
 
 const getProductAll = async({pages}) => {
+    const params = urlParamsGenerate({pages})
 
+    const res = await axios.get(`${API_URL}/products${params}`, {
+        headers: { Authorization: token },
+    })
+
+    return res
 }
 
 const getProductId = async({idProduct}) => {
     const res = await axios.get(`${API_URL}/products/${idProduct}/`, {
-        headers: { Authorization: `Token ${getDataFromLocalStorage("user")?.auth_token}` },
+        headers: { Authorization: token },
     })
 
     return res
