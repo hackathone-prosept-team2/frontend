75cf9a01d6ee4809518423595faa118c201a3218
Максим Таланов <mail@maxtalanov.ru>
2023-12-07 15:11:58 +0300

    feat(slice): Add pricesSlice file

--- src/Features/Prices/pricesSlice.js
+++ src/Features/Prices/pricesSlice.js
@@ -8,11 +8,53 @@ const initialState = {
     message: "",
 }
 
+export const addPrices =  createAsyncThunk("prices/add", async(tunnkAPI)=> {
+    try {
+      return await pricesService.addPrices()
+    } catch (error) {
+      return tunnkAPI.rejectWithValue(error?.response?.data)
+    }
+})
+
+export const deletePrices =  createAsyncThunk("prices/add", async(tunnkAPI)=> {
+    try {
+      return await pricesService.addPrices()
+    } catch (error) {
+      return tunnkAPI.rejectWithValue(error?.response?.data)
+    }
+})
+
 const pricesSlice = createSlice({
     name: "prices",
     initialState,
     reducers: {},
-    extraReducers: (builder) => { return null }
+    extraReducers: (builder) => { 
+        builder
+        .addCase(addPrices.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(addPrices.fulfilled, (state) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+        })
+        .addCase(addPrices.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+        })
+        .addCase(deletePrices.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(deletePrices.fulfilled, (state) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+        })
+        .addCase(deletePrices.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+        })
+    },
 })
   
 export default pricesSlice.reducer
