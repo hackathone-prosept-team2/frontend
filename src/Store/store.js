46d5d7293c670efe8c9588bc2ff44e0a365fc44a
Максим Таланов <mail@maxtalanov.ru>
2023-12-07 15:15:49 +0300

    feat(store): Add prices to the store

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { authReducer } from 'Features';
+import { authReducer, pricesReduser } from 'Features';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    prices: pricesReduser,
   },
 });
 
