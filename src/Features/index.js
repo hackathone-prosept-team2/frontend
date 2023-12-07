bd6e15cf9a8cb66b534c184badc825299d7e8339
Максим Таланов <mail@maxtalanov.ru>
2023-12-07 03:17:44 +0300

    feat(services): Add services to indexing
    Dealers
    Keys
    Prices
    Products

--- src/Features/index.js
+++ src/Features/index.js
@@ -2,3 +2,15 @@
 export { signUp, login, tokenCheck, logout } from "./Auth/authSlice"
 export { default as authReducer } from "./Auth/authSlice"
 export { default as authService } from "./Auth/authService"
+
+// Dealers
+export { default as dealersService } from "./Dealers/dealersServices"
+
+// Keys
+export { default as keysService } from "./Keys/keysService"
+
+// Prices
+export { default as pricesService } from "./Prices/pricesService"
+
+// Products
+export { default as productsService } from "./Products/productsService"
