d352596ace8cd06bb50b4eb98cfb73cf3e29b8c9
Максим Таланов <mail@maxtalanov.ru>
2023-12-07 04:05:18 +0300

    feat(slice): Include slices in indexing in a commented-out form
    - Dealers
    - Keys
    - Prices
    - Products

--- src/Features/index.js
+++ src/Features/index.js
@@ -5,12 +5,20 @@ export { default as authService } from "./Auth/authService"
 
 // Dealers
 export { default as dealersService } from "./Dealers/dealersServices"
+// export {  } from "./Dealers/dealersSlice"
+// export { dafault as dealersReducer } from "./Dealers/dealersSlice"
 
 // Keys
 export { default as keysService } from "./Keys/keysService"
+// export {  } from "./Keys/keysSlice"
+// export { dafault as keysReduser } from "./Keys/keysSlice"
 
 // Prices
 export { default as pricesService } from "./Prices/pricesService"
+// export {  } from "./Prices/pricesSlice"
+// export { dafault as pricesReduser } from "./Prices/pricesSlice"
 
 // Products
 export { default as productsService } from "./Products/productsService"
+// export {  } from "./Products/productsSlice"
+// export { dafault as productsReduser } from "./Products/productsSlice"
